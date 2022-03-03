7f35b6e314177f79216bc6126de628c46f7cc413
yash <yaswanth.k23@gmail.com>
2022-03-03 22:09:12 +0530

    blocking and non blocking

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const fs = require("fs");
 const text = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(text);
 
+// Blocking, synchronous way
 // usage of template string literals to plug in the text
 const textOut = `This is what we know about Avocado: ${text}.\n Created on ${Date.now()}`;
 fs.writeFileSync("./txt/output.txt", textOut);
 console.log("File created");
+
+// Non-blocking asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  console.log(data);
+  fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data1) => {
+    console.log(data1);
+  });
+});
+console.log("read file is going on......");
