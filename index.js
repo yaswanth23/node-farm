2c217e40faf039f1a03feb1fbc194f311eb60d2e
yash <yaswanth.k23@gmail.com>
2022-03-08 17:29:14 +0530

    seperating modules

--- index.js
+++ index.js
@@ -29,20 +29,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
+const replaceTemplate = require("./modules/replaceTemplate");
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -78,8 +65,8 @@ const server = http.createServer((req, res) => {
     //Product page
   } else if (pathname === "/product") {
     res.writeHead(200, {
-      "content-type": 'text/html'
-    })
+      "content-type": "text/html",
+    });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
     res.end(output);
